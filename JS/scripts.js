0dd7de3e1b1d535fb0ce2b2696e395444c58e598
DYNNIwav <paalomei@gmail.com>
2025-09-21 16:22:47 +0200

    project structure and science museum site
    Add initial files for a science museum website, including HTML pages (home, exhibitions, tickets, visit us, maintenance), CSS for layout and components, JavaScript for dynamic opening hours and hamburger menu, and optimized images. Update README with project overview and features.

---
+++ JS/scripts.js
@@ -0,0 +1,29 @@
+console.log('JavaScript file loaded');
+
+document.addEventListener("DOMContentLoaded", () => {
+    const openingHours = document.getElementById("opening-hours");
+    if (openingHours) {
+const today = new Date().getDay(); 
+const hours = {
+    0: "10:00 - 18:00",
+    1: "stengt",
+    2: "10:00 - 17:00",
+    3: "10:00 - 17:00",
+    4: "10:00 - 17:00",
+    5: "10:00 - 17:00",
+    6: "10:00 - 18:00",
+    };
+    openingHours.textContent = hours[today];
+    }
+
+
+
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const sidebar = document.querySelector('.header-container');
+
+    hamburgerMenu.addEventListener('click', () => {
+        hamburgerMenu.classList.toggle('open');
+            sidebar.classList.toggle('open');
+    });
+});
+
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# semester-project1--Paal-Omland-Eilevstjoenn
+# Semester project 1 - Pål Omland Eilevstjønn
 
+## Project overview
+
+interactive science museum website designed for children aged 7-15 and their parents. The website is built with HTML, CSS and some javascript.
+
+## features
+
+- 5 responsive pages for desktop, tablets and mobile
+- showcase for exhibitions
+- optimized images
+- WCAG accessibility
+- SEO optimization
+
+## Pages
+
+- home (index.html)
+- exhibitions (utstillingar.html)
+- tickets (billettar.html)
+- visit us (besok-oss.html)
+- added a maintenance page for no broken links. (maintenance.html)
